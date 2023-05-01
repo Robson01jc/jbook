3b9717d949db18fc2c7d7d39fec6361950c0a7e3
Robson Carvalho <robson01jc@gmail.com>
2023-05-01 17:23:35 -0300

    binding react and redux together

---
+++ src/components/cell-list-item.tsx
@@ -0,0 +1,26 @@
+import { Cell } from '../state';
+import ActionBar from './action-bar';
+import CodeCell from './code-cell';
+import TextEditor from './text-editor';
+
+interface CellListItemProps {
+  cell: Cell;
+}
+
+const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+  const child: JSX.Element =
+    cell.type === 'code' ? (
+      <CodeCell cell={cell} />
+    ) : (
+      <TextEditor cell={cell} />
+    );
+
+  return (
+    <div>
+      <ActionBar id={cell.id} />
+      {child}
+    </div>
+  );
+};
+
+export default CellListItem;
