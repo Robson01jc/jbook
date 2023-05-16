6f44f4d8eccb96f136f8a7af1979546a3c84e546
Robson Carvalho <robson01jc@gmail.com>
2023-05-15 21:29:28 -0300

    action bar and add cell components

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,5 +1,6 @@
 import { Cell } from '../state';
 import ActionBar from './action-bar';
+import './cell-list-item.css';
 import CodeCell from './code-cell';
 import TextEditor from './text-editor';
 
@@ -8,19 +9,26 @@ interface CellListItemProps {
 }
 
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
-  const child: JSX.Element =
-    cell.type === 'code' ? (
-      <CodeCell cell={cell} />
-    ) : (
-      <TextEditor cell={cell} />
+  let child: JSX.Element;
+  if (cell.type === 'code') {
+    child = (
+      <>
+        <div className="action-bar-wrapper">
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
     );
+  } else {
+    child = (
+      <>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </>
+    );
+  }
 
-  return (
-    <div>
-      <ActionBar id={cell.id} />
-      {child}
-    </div>
-  );
+  return <div className="cell-list-item">{child}</div>;
 };
 
 export default CellListItem;
