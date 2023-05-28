1abbc67f2fe8f6a13c29e0774feff3354d5406d1
Robson Carvalho <robson01jc@gmail.com>
2023-05-28 15:58:39 -0300

    change insert_cell_before to insert_cell_after

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -2,22 +2,22 @@ import { useActions } from '../hooks/use-actions';
 import './add-cell.css';
 
 interface AddCellProps {
-  nextCellId: string | null;
+  previousCellId: string | null;
   forceVisible?: boolean;
 }
 
 const AddCell: React.FC<AddCellProps> = ({
   forceVisible = false,
-  nextCellId,
+  previousCellId,
 }) => {
-  const { insertCellBefore } = useActions();
+  const { insertCellAfter } = useActions();
 
   return (
     <div className={`add-cell ${forceVisible && 'force-visible'}`}>
       <div className="add-buttons">
         <button
           className="button is-rounded is-primary is-small"
-          onClick={() => insertCellBefore(nextCellId, 'code')}
+          onClick={() => insertCellAfter(previousCellId, 'code')}
         >
           <span className="icon is-small">
             <i className="fas fa-plus" />
@@ -26,7 +26,7 @@ const AddCell: React.FC<AddCellProps> = ({
         </button>
         <button
           className="button is-rounded is-primary is-small"
-          onClick={() => insertCellBefore(nextCellId, 'text')}
+          onClick={() => insertCellAfter(previousCellId, 'text')}
         >
           <span className="icon is-small">
             <i className="fas fa-plus" />
