f52f770a517e2366bc1582031f697d4cb7b8b1ce
Robson Carvalho <robson01jc@gmail.com>
2023-06-08 12:26:21 -0300

    cumulative code execution

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,8 +1,8 @@
-import './code-cell.css';
 import React, { useEffect } from 'react';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import { Cell } from '../state';
+import './code-cell.css';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import Resizable from './resizable';
@@ -17,22 +17,50 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return bundles[cell.id];
   });
   const isBundleFound = !!bundle;
+  const cumulativeCode = useTypedSelector((state) => {
+    const { data, order } = state.cells;
+    const orderedCells = order.map((cellId) => data[cellId]);
+    const setup = `
+      import _React from 'react';
+      import _ReactDOM from 'react-dom';
+
+      const show = (value) => {
+        const root = document.querySelector('#root');
+
+        if (typeof value === 'object') {
+          if (value.$$typeof && value.props) {
+            _ReactDOM.render(value, root);
+          } else {
+            root.innerHTML = JSON.stringify(value);
+          }
+        } else {
+          root.innerHTML = value;
+        }
+      };
+    `;
+    const cumulativeCode = orderedCells
+      .slice(0, order.indexOf(cell.id) + 1)
+      .filter((cell) => cell.type === 'code')
+      .map((cell) => cell.content);
+
+    return [setup, ...cumulativeCode].join('\n');
+  });
 
   useEffect(() => {
     if (!isBundleFound) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
 
       return;
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
     }, 1000);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [cell.content, cell.id, createBundle, isBundleFound]);
+  }, [cumulativeCode, cell.id, createBundle, isBundleFound]);
 
   return (
     <Resizable direction="vertical">
